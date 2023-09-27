8c4dce1e3eae33a5a7d3b300fb05a448a2628aa0
SCOTT DIERBECK <scott.dierbeck@gmail.com>
2023-09-27 15:48:50 -0700

    Initial commit of SpringBoot generated code

---
+++ src/main/java/com/example/rpws/chapters/SpringBootAwesome/SpringBootAwesomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpws.chapters.SpringBootAwesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwesomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwesomeApplication.class, args);
+	}
+
+}
